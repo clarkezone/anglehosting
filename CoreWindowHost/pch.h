12e7dd860b50314464c35449c2758e93e4d04873
James Clarke (WDG) <jeclarke@ntdev.microsoft.com>
2019-12-04 12:58:06 -0800

    Make the UWP project build again

--- CoreWindowHost/pch.h
+++ CoreWindowHost/pch.h
@@ -7,12 +7,14 @@
 
 #define NOMINMAX
 
-#include "winrt/Windows.ApplicationModel.Core.h"
-#include "winrt/Windows.UI.Core.h"
-#include "winrt/Windows.UI.Composition.h"
-#include "winrt/Windows.UI.Input.h"
-#include "winrt/Windows.Foundation.h"
-#include "winrt/Windows.Foundation.Collections.h"
+
+#include <windows.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.ApplicationModel.Core.h>
+#include <winrt/Windows.UI.Core.h>
+#include <winrt/Windows.UI.Composition.h>
+#include <winrt/Windows.UI.Input.h>
 #include "winrt/Windows.System.Threading.h"
 
 #include <concrt.h>
