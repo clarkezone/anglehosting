6221e13e3d8c2d4ea07b76d8bcf9b15072357d4f
James Clarke (WDG) <jeclarke@ntdev.microsoft.com>
2019-12-10 18:18:10 -0800

    builds from locally built binaries

---
+++ Scripts/copylocal.bat
@@ -0,0 +1,25 @@
+set ROOT=C:\src\github.com\clarkezone\angle
+set UWPROOT=%ROOT%\out\DebugUWP\
+set WIN32ROOT=%ROOT%\out\DebugWin32\
+
+IF not EXIST C:\src\github.com\clarkezone\anglehosting\dependencies (mkdir C:\src\github.com\clarkezone\anglehosting\dependencies)
+IF not EXIST C:\src\github.com\clarkezone\anglehosting\dependencies\Angle (mkdir C:\src\github.com\clarkezone\anglehosting\dependencies\ANGLE)
+IF not EXIST C:\src\github.com\clarkezone\anglehosting\dependencies\Angle (mkdir C:\src\github.com\clarkezone\anglehosting\dependencies\ANGLE\Include)
+IF not EXIST C:\src\github.com\clarkezone\anglehosting\dependencies\UWPBin (mkdir C:\src\github.com\clarkezone\anglehosting\dependencies\ANGLE\UWPBin)
+IF not EXIST C:\src\github.com\clarkezone\anglehosting\dependencies\UWPBin (mkdir C:\src\github.com\clarkezone\anglehosting\dependencies\ANGLE\UWPLib)
+IF not EXIST C:\src\github.com\clarkezone\anglehosting\dependencies\Win32Bin (mkdir C:\src\github.com\clarkezone\anglehosting\dependencies\ANGLE\Win32Bin)
+IF not EXIST C:\src\github.com\clarkezone\anglehosting\dependencies\Win32Bin (mkdir C:\src\github.com\clarkezone\anglehosting\dependencies\ANGLE\Win32Lib)
+
+xcopy %ROOT%\include\*.* C:\src\github.com\clarkezone\anglehosting\dependencies\Angle\Include\ /s /e
+
+copy %UWPROOT%libGLESv2.dll C:\src\github.com\clarkezone\anglehosting\dependencies\Angle\UWPBin\.
+copy %UWPROOT%libGLESv2.dll.pdb C:\src\github.com\clarkezone\anglehosting\dependencies\Angle\UWPBin\.
+copy %UWPROOT%libEGL.dll C:\src\github.com\clarkezone\anglehosting\dependencies\Angle\UWPBin\.
+copy %UWPROOT%libEGL.dll.pdb C:\src\github.com\clarkezone\anglehosting\dependencies\Angle\UWPBin\.
+
+copy %UWPROOT%libEGL.dll.lib C:\src\github.com\clarkezone\anglehosting\dependencies\Angle\UWPLib\.
+copy %UWPROOT%libGLESv2.dll.lib C:\src\github.com\clarkezone\anglehosting\dependencies\Angle\UWPLib\.
+
+
+copy %WIN32ROOT%libEGL.dll C:\src\github.com\clarkezone\anglehosting\dependencies\Angle\Win32Bin\.
+copy %WIN32ROOT%libEGL.dll.pdb C:\src\github.com\clarkezone\anglehosting\dependencies\Angle\Win32Bin\.
---
+++ Scripts/createangletargets.bat
@@ -0,0 +1,12 @@
+setlocal EnableDelayedExpansion
+
+pushd C:\src\github.com\clarkezone\angle
+set DEPOT_TOOLS_WIN_TOOLCHAIN=0
+IF not EXIST Out (mkdir Out)
+if not EXIST Out\DebugUWP (mkdir Out\DebugUWP)
+if not EXIST Out\DebugWin32 (mkdir Out\DebugWin32)
+echo # Set build arguments here. See `gn buildargs`. > Out\DebugUWP\args.gn
+echo target_os = "winuwp" >> Out\DebugUWP\args.gn
+echo is_clang = false^ >> Out\DebugUWP\args.gn
+gn gen out\debuguwp --sln=angle-debug --ide=vs2019
+popd
