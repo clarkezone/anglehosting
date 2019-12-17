ec1a0133a1c38017939163f10f69562f4cf27821
James Clarke (WDG) <jeclarke@microsoft.com>
2019-12-16 17:38:19 -0800

    Change branch

--- Scripts/getangle.bat
+++ Scripts/getangle.bat
@@ -3,6 +3,7 @@ IF not EXIST C:\src\github.com (mkdir C:\src\github.com)
 IF not EXIST C:\src\github.com\clarkezone (mkdir C:\src\github.com\clarkezone)
 pushd C:\src\github.com\clarkezone
 call git clone https://github.com/clarkezone/angle.git
+call git checkout UniversalCompositorNativeWindow
 cd angle
 Set DEPOT_TOOLS_WIN_TOOLCHAIN=0
 call python scripts/bootstrap.py
