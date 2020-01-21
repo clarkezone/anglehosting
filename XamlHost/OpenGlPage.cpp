d5358978bb7d262cd041f83d80163212b4b31b98
Johan Lindfors <johan.lindfors@coderox.se>
2020-01-21 09:34:50 +0100

    XamlHost sample

---
+++ XamlHost/OpenGlPage.cpp
@@ -0,0 +1,47 @@
+﻿#include "pch.h"
+#include "OpenGlPage.h"
+#include "OpenGlPage.g.cpp"
+
+using namespace winrt;
+
+using namespace Windows;
+using namespace Windows::ApplicationModel::Core;
+using namespace Windows::Foundation::Numerics;
+using namespace Windows::UI;
+using namespace Windows::UI::Core;
+using namespace Windows::UI::Composition;
+using namespace Windows::UI::Xaml::Controls;
+
+namespace winrt::XamlHost::implementation
+{
+    OpenGlPage::OpenGlPage()
+    {
+        InitializeComponent();
+        //Compositor compositor;
+        //ContainerVisual root = compositor.CreateContainerVisual();
+        //m_target = compositor.CreateTargetForCurrentView();
+        //m_target.Root(root);
+        //m_visuals = root.Children();
+
+        AddVisual({ 10.0f, 10.0f });
+    }
+
+    void OpenGlPage::AddVisual(float2 const point)
+    {
+        //Compositor compositor = m_visuals.Compositor();
+        //SpriteVisual visual = compositor.CreateSpriteVisual();
+        //visual.Size(
+        //    {
+        //        600.0f,
+        //        600.0f
+        //    });
+
+        ////visual.Brush(compositor.CreateColorBrush(Windows::UI::Colors::Red()));
+
+        //m_visuals.InsertAtTop(visual);
+        SwapChainPanel panel;
+        Content(panel);
+        m_render.SetSwapChainPanel(panel);
+        m_render.Start();
+    }
+}
---
+++ XamlHost/OpenGlPage.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "OpenGlPage.g.h"
+
+namespace winrt::XamlHost::implementation
+{
+    struct OpenGlPage : OpenGlPageT<OpenGlPage>
+    {
+        OpenGlPage();
+
+    private:
+        void AddVisual(winrt::Windows::Foundation::Numerics::float2 const point);
+
+        winrt::Windows::UI::Composition::CompositionTarget m_target{ nullptr };
+        winrt::Windows::UI::Composition::VisualCollection m_visuals{ nullptr };
+        AngleSpriteRenderer m_render;
+    };
+}
+
+namespace winrt::XamlHost::factory_implementation
+{
+    struct OpenGlPage : OpenGlPageT<OpenGlPage, implementation::OpenGlPage>
+    {
+    };
+}
